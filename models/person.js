996215f57163df7ea3c163bee77a8f57b9612b15
kainulj <jaakko.kainulainen@aalto.fi>
2020-12-16 21:38:55 +0200

    Osan 3 tehtävät

--- models/person.js
+++ models/person.js
@@ -6,11 +6,11 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {    
-    console.log('connected to MongoDB')  
-  })  
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)  
+    console.log('error connecting to MongoDB:', error.message)
   })
 
 const personSchema = new mongoose.Schema({
